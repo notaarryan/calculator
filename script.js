215cc3e815f3a0a1715067d5a5bece8b8e56d473
Aryan Parmar <notaarryan@users.noreply.github.com>
2025-05-30 19:42:28 -0400

    Built js display and operation functionality
    - Added event listeners to numbered and operation and backspace except decimal buttons to update display when clicked
    - Also now clicking buttons updates num1 or num2 or operator according to operationCount
    - Added functionality to equals button which calls operate function and does the math and updates the display

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let multiply = (a, b) => {
 };
 
 let divide = (a, b) => {
+  console.log(a / b);
   return a / b;
 };
 
@@ -19,9 +20,9 @@ let operate = (num1, num2, operator) => {
     return add(num1, num2);
   } else if (operator == "-") {
     return subtract(num1, num2);
-  } else if (operator == "*") {
+  } else if (operator == "X") {
     return multiply(num1, num2);
-  } else if (operator == "/") {
+  } else if (operator == "÷") {
     return divide(num1, num2);
   } else {
     return 0;
@@ -34,8 +35,57 @@ let clear = () => {
   operator = "";
 };
 
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operator = "";
+let operationCount = 0;
+let result = 0;
 
-let 
+let numbers = document.querySelectorAll(".btn-number");
+let decimal = document.querySelector("#op-decimal");
+let display = document.querySelector(".display");
+let ac = document.querySelector("#op-ac");
+let backspace = document.querySelector("#op-backspace");
+let operations = document.querySelectorAll(".operators .btn-operator");
+let equals = document.querySelector("#op-equals");
+
+ac.addEventListener("click", () => {
+  num1 = "";
+  num2 = "";
+  operator = "";
+  display.innerText = ``;
+  operationCount = 0;
+});
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    if (operationCount != 1) {
+      num1 += number.id;
+      display.innerText = `${num1}`;
+    } else if (operationCount == 1) {
+      num2 += number.id;
+      display.innerText = `${num1} ${operator} ${num2}`;
+    }
+  });
+});
+
+operations.forEach((op) => {
+  op.addEventListener("click", () => {
+    if (operationCount === 0 && op.id != "op-ac") {
+      operator = op.innerText;
+      display.innerText = `${num1} ${operator}`;
+      operationCount++;
+    }
+  });
+});
+
+equals.addEventListener("click", () => {
+  result = operate(parseInt(num1), parseInt(num2), operator);
+  if (num1 != "" && num2 != "" && operator != "") {
+    console.log(num1, num2);
+    display.innerText = `${result}`;
+    num1 = result.toString();
+    num2 = "";
+    operationCount = 0;
+  }
+});
